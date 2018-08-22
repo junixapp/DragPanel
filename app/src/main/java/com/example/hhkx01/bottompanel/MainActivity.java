b5daf72f3a812639523e4674cf0f30e43d0131e0
lixiaojun <16167479@qq.com>
2018-08-22 11:23:47 +0800

    add click sample

--- app/src/main/java/com/example/hhkx01/bottompanel/MainActivity.java
+++ app/src/main/java/com/example/hhkx01/bottompanel/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final DragPanel bottomPanel = (DragPanel) findViewById(R.id.bottomPanel);
-
+        final View tv1 = findViewById(R.id.tv1);
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,24 +41,43 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        final View view = findViewById(R.id.iv);
-        view.setOnClickListener(new View.OnClickListener() {
+        final View view = findViewById(R.id.headerView);
+        //can not set header click listener like this.
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Click headerView", Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+
+        bottomPanel.setOnTapListener(new DragPanel.OnHeaderClickListener() {
+            @Override
+            public void onHeaderClick() {
+                Log.e(TAG, "onClick: Header");
+//                Toast.makeText(MainActivity.this, "Click Header", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        tv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
-
+                Log.e(TAG, "onClick: tv1");
+//                Toast.makeText(MainActivity.this, "Click tv1", Toast.LENGTH_LONG).show();
             }
         });
 
         bottomPanel.setOnPanelDragListener(new DragPanel.OnPanelDragListener() {
             @Override
             public void onOpen() {
-                Toast.makeText(MainActivity.this, "open", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "onClick: onOpen");
+//                Toast.makeText(MainActivity.this, "open", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onClose() {
-                Toast.makeText(MainActivity.this, "close", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "onClick: onClose");
+//                Toast.makeText(MainActivity.this, "close", Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -67,12 +86,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final View tv1 = findViewById(R.id.tv1);
+
 
         view.postDelayed(new Runnable() {
             @Override
             public void run() {
-                tv1.setVisibility(View.GONE);
+//                tv1.setVisibility(View.GONE);
             }
         }, 2000);
 
